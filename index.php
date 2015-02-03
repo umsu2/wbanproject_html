b6f0d77786208ca5fbb170e4560a628b4b4ba2a7
Yang <umsu2@myumanitoba.ca>
2015-02-02 20:55:37 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yang
+ * Date: 01/02/2015
+ * Time: 3:03 PM
+ */
+$docRoot = getenv("DOCUMENT_ROOT");
+
+?>
+
+<h1>ECE 4600 Capstone Project on wireless body area network</h1>
+
+<a href="/webservice/login.php">Goto Log in Screen</a>
